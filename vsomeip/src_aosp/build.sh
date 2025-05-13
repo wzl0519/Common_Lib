2eafd73d26638cfc7e1408d25ee467b886011c7d
zhongzhi <zhifei.zhong@forvia.com>
2025-05-13 13:57:56 +0800

    add aosp source

---
+++ vsomeip/src_aosp/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+if [ ! -d "build" ];then
+  mkdir build
+  else
+  rm -r build
+  mkdir build
+ fi
+
+basedir=`cd $(dirname $0); pwd -P`
+cd $basedir/build
+pwd
+
+cmake -DCMAKE_INSTALL_PREFIX=/home/taohj_86/temp/project/common_api/bin/vsomeip-master ..
+make -j20
+#make examples -j8
+make install
